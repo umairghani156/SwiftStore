7e35ef913846a9cfcebc553937633389e831b7bd
umairghani156 <umairghani156@gmail.com>
2025-05-19 23:44:15 -0700

    frontend URL added.

--- BMS/server/index.js
+++ BMS/server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173","https://zeta-bms.vercel.app"],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true, // Allow credentials (cookies, authorization headers)
         allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
